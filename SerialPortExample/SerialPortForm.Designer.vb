19bb1ea33f8830f71974b781fb0d407c12c9d7ce
Jason Permann <JaceFoxman@gmail.com>
2025-10-16 11:25:47 -0600

    Add project files.

---
+++ SerialPortExample/SerialPortForm.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SerialPortForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.Timer = New System.Windows.Forms.Timer(Me.components)
+        Me.Close_Button = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'SerialPort1
+        '
+        Me.SerialPort1.PortName = "COM3"
+        '
+        'Timer
+        '
+        Me.Timer.Interval = 500
+        '
+        'Close_Button
+        '
+        Me.Close_Button.Location = New System.Drawing.Point(259, 319)
+        Me.Close_Button.Name = "Close_Button"
+        Me.Close_Button.Size = New System.Drawing.Size(272, 78)
+        Me.Close_Button.TabIndex = 0
+        Me.Close_Button.Text = "CLOSE"
+        Me.Close_Button.UseVisualStyleBackColor = True
+        '
+        'SerialPortForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Close_Button)
+        Me.Name = "SerialPortForm"
+        Me.Text = "SerialPortForm"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents Timer As Timer
+    Friend WithEvents Close_Button As Button
+End Class
---
+++ SerialPortExample/SerialPortForm.vb
@@ -0,0 +1,76 @@
+﻿Public Class SerialPortForm
+    Private Sub SerialPortForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Connect()
+        Timer.Interval = 500 'Set Timer interval to 500 ms
+        Timer.Start()
+        'for each timer tick, call ShiftLED
+    End Sub
+
+    Sub Connect()
+        SerialPort1.Close()
+        SerialPort1.BaudRate = 115200 'Q@ Board Default
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8
+        SerialPort1.PortName = "COM3" 'Change to your COM Port
+
+        SerialPort1.Open()
+        If SerialPort1.IsOpen Then
+            MessageBox.Show("Connected to " & SerialPort1.PortName)
+        End If
+        SerialPort1.Close()
+    End Sub
+
+    Private Sub Close_Button_Click(sender As Object, e As EventArgs) Handles Close_Button.Click
+        SerialPort1.Close()
+        Me.Close()
+    End Sub
+
+    Sub ShiftLED()
+        SerialPort1.Close()
+        SerialPort1.BaudRate = 115200 'Q@ Board Default
+        SerialPort1.Parity = IO.Ports.Parity.None
+        SerialPort1.StopBits = IO.Ports.StopBits.One
+        SerialPort1.DataBits = 8
+        SerialPort1.PortName = "COM3" 'Change to your COM Port
+        SerialPort1.Open()
+        Dim data As Byte() = New Byte(1) {}
+        data(0) = &H20 'First byte
+        Dim count = CountIncrease() 'Increase count
+        Select Case count
+            Case 1
+                data(1) = &H1
+            Case 2
+                data(1) = &H2
+            Case 3
+                data(1) = &H4
+            Case 4
+                data(1) = &H8
+            Case 5
+                data(1) = &H10
+            Case 6
+                data(1) = &H20
+            Case 7
+                data(1) = &H40
+            Case 8
+                data(1) = &H80
+        End Select
+
+
+        SerialPort1.Write(data, 0, 2) 'Send 2 bytes of data
+        SerialPort1.Close()
+    End Sub
+
+    Function CountIncrease() As Integer
+        Static count As Integer = 1
+        count += 1
+        If count > 8 Then
+            count = 1
+        End If
+        Return count
+    End Function
+
+    Private Sub Timer_Tick(sender As Object, e As EventArgs) Handles Timer.Tick
+        ShiftLED()
+    End Sub
+End Class
